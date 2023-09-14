71a928823ca941cd2765aa70a417e7b0ddf46638
Peter Serwylo <peter@serwylo.com>
2023-09-15 08:04:51 +1000

    Tweak templates to look less like app skeleton and more like an example app.

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -41,8 +41,11 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
             <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
         </div>
         <div class="top-nav-links">
-            <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Documentation</a>
-            <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
+            <?= $this->Html->link('Articles', ['controller' => 'articles', 'action' => 'index']) ?>
+            ::
+            <?= $this->Html->link('Tags', ['controller' => 'tags', 'action' => 'index']) ?>
+            ::
+            <?= $this->Html->link('Users', ['controller' => 'users', 'action' => 'index']) ?>
         </div>
     </nav>
     <main class="main">
